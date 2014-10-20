f9c10b1aceeb35fab833da734eeeed801eff3b88
Yuri Artemev <i@artemeff.com>
2014-10-21 00:02:14 +0400

    add std:timestamp/0

--- src/std.erl
+++ src/std.erl
@@ -1,1 +1,15 @@
 -module(std).
+-export([timestamp/0]).
+
+timestamp() ->
+    {Mega, Seconds, _} = now(),
+    Mega * 1000000 + Seconds.
+
+%%
+%% Test
+%%
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+-endif.
