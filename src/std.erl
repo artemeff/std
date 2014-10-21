f63ffc0b6fddfb223b6da43ad9efeaf9ac214b92
Yuri Artemev <i@artemeff.com>
2014-10-21 14:39:06 +0400

    add std:priv_dir/1

--- src/std.erl
+++ src/std.erl
@@ -1,9 +1,17 @@
 -module(std).
--export([timestamp/0]).
+-export([ timestamp/0
+        , priv_dir/1
+        ]).
 
 timestamp() ->
     {Mega, Seconds, _} = now(),
     Mega * 1000000 + Seconds.
+
+-spec priv_dir(atom())
+   -> string().
+priv_dir(App) ->
+   Ebin = filename:dirname(code:which(App)),
+   filename:join(filename:dirname(Ebin), "priv").
 
 %%
 %% Test
