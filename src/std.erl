102355aab94497aa3f91dc43b7b8b36d44c1a6c5
Yuri Artemev <i@artemeff.com>
2014-12-12 14:12:03 +0300

    add chunk_list/2,3

--- src/std.erl
+++ src/std.erl
@@ -3,6 +3,7 @@
         , priv_dir/1
         , seconds_in/1
         , generate_random_string/1
+        , chunk_list/2, chunk_list/3
         ]).
 
 -spec timestamp()
@@ -43,11 +44,38 @@ generate_random_string(Bin, Rem) ->
     Next = random:uniform(62) - 1,
     generate_random_string(<<Bin/binary, Next>>, Rem - 1).
 
+-spec chunk_list(list(), non_neg_integer())
+   -> [[any(), ...]].
+chunk_list(List, Len) ->
+    chunk_list(List, Len, undefined).
+-spec chunk_list(list(), non_neg_integer(), any())
+   -> [[any(), ...]].
+chunk_list(List, Len, Placeholder) ->
+    LeaderLength = case length(List) rem Len of
+        0 -> 0;
+        N -> Len - N
+    end,
+    Leader = lists:duplicate(LeaderLength, Placeholder),
+    chunk_list(Leader ++ lists:reverse(List), [], 0, Len).
+chunk_list([], Acc, _, _) -> Acc;
+chunk_list([H|T], Acc, Pos, Max) when Pos == Max ->
+    chunk_list(T, [[H] | Acc], 1, Max);
+chunk_list([H|T], [HAcc | TAcc], Pos, Max) ->
+    chunk_list(T, [[H | HAcc] | TAcc], Pos + 1, Max);
+chunk_list([H|T], [], Pos, Max) ->
+    chunk_list(T, [[H]], Pos + 1, Max).
+
 %%
 %% Test
 %%
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
+
+chunk_list_test() ->
+    ?assertEqual([[1, 2, 3], [4, 5, undefined]],
+        chunk_list([1, 2, 3, 4, 5], 3)),
+    ?assertEqual([[1, 2, 3], [4, 5, zero]],
+        chunk_list([1, 2, 3, 4, 5], 3, zero)).
 
 -endif.
