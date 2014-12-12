a2ab8076e8155a629ecf9288a2c0c10c7a3da990
Yuri Artemev <i@artemeff.com>
2014-12-12 13:59:56 +0300

    add generate_random_string/1

--- src/std.erl
+++ src/std.erl
@@ -1,6 +1,7 @@
 -module(std).
 -export([ timestamp/0, timestamp/1
         , priv_dir/1
+        , generate_random_string/1
         ]).
 
 -spec timestamp()
@@ -18,6 +19,20 @@ timestamp({Mega, Seconds, _}) ->
 priv_dir(App) ->
    Ebin = filename:dirname(code:which(App)),
    filename:join(filename:dirname(Ebin), "priv").
+
+-spec generate_random_string(non_neg_integer())
+   -> binary().
+generate_random_string(Size) when Size > 0 ->
+    {X,Y,Z} = now(), random:seed(X,Y,Z),
+    Initial = random:uniform(62) - 1,
+    generate_random_string(<<Initial>>, Size - 1);
+generate_random_string(_) -> <<>>.
+generate_random_string(Bin, 0) ->
+    Chars = <<"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890">>,
+    << <<(binary_part(Chars, B, 1))/binary>> || <<B>> <= Bin >>;
+generate_random_string(Bin, Rem) ->
+    Next = random:uniform(62) - 1,
+    generate_random_string(<<Bin/binary, Next>>, Rem - 1).
 
 %%
 %% Test
